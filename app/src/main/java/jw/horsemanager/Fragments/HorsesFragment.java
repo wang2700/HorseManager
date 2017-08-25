69cf294c8f706999624f038e5a1927285681b46d
jerry <jerrywangpico@gmail.com>
2017-08-24 21:17:44 -0400

    initial commit

--- app/src/main/java/jw/horsemanager/Fragments/HorsesFragment.java
+++ app/src/main/java/jw/horsemanager/Fragments/HorsesFragment.java
@@ -3,6 +3,8 @@ package jw.horsemanager.Fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +17,16 @@ import jw.horsemanager.R;
 
 public class HorsesFragment extends Fragment {
 
+    private Toolbar mToolBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.horses_fragment, container, false);
+
+        mToolBar = view.findViewById(R.id.toolbar);
+
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolBar);
+
         return view;
     }
 }
